58950f42508bea0806f5818bf6163517791b4d15
KonkenBonken <konrad05pettersson@gmail.com>
2023-01-03 03:02:17 +0100

    Export instance of Board instead of class

--- src/Board.tsx
+++ src/Board.tsx
@@ -6,51 +6,53 @@ import Corner from './Corner'
 import Edge from './Edge'
 import { Numbers, TileResources, TileWidths, Corners, Edges } from './utils/BoardData'
 
-export default class Board {
-  tiles = Object.seal(
-    shuffle(TileResources.map((resource, i) => new Tile(resource, Numbers[i]))),
-  )
-
-  corners = Object.seal(
-    Corners.map(length =>
-      Object.seal(
-        Array.from({ length }, () => new Corner())
-      )))
-
-  edges = Object.seal(
-    Edges.map(length =>
-      Object.seal(
-        Array.from({ length }, () => new Edge())
-      )))
-
-  render() {
-    return (<div className={cls('board')}>
-      <div className={cls('corners')}>
-        {
-          this.corners.map(cornerRow => <div>
-            {cornerRow.map(corner => corner.render())}
-          </div>)
-        }
-      </div>
-      <div className={cls('edges')}>
-        {
-          this.edges.map(edgeRow => <div>
-            {edgeRow.map(edge => edge.render())}
-          </div>)
-        }
-      </div>
-      <div className={cls('tiles')}>
-        {
-          TileWidths.map(([start, width], rowIndex) =>
-            <div key={rowIndex} >
-              {
-                this.tiles.slice(start, start + width).map(tile => tile.render())
-              }
-            </div>
-          )
-        }
-      </div>
-    </div>
+export default new (
+  class Board {
+    tiles = Object.seal(
+      shuffle(TileResources.map((resource, i) => new Tile(resource, Numbers[i]))),
     )
+
+    corners = Object.seal(
+      Corners.map(length =>
+        Object.seal(
+          Array.from({ length }, () => new Corner())
+        )))
+
+    edges = Object.seal(
+      Edges.map(length =>
+        Object.seal(
+          Array.from({ length }, () => new Edge())
+        )))
+
+    render() {
+      return (<div className={cls('board')}>
+        <div className={cls('corners')}>
+          {
+            this.corners.map(cornerRow => <div>
+              {cornerRow.map(corner => corner.render())}
+            </div>)
+          }
+        </div>
+        <div className={cls('edges')}>
+          {
+            this.edges.map(edgeRow => <div>
+              {edgeRow.map(edge => edge.render())}
+            </div>)
+          }
+        </div>
+        <div className={cls('tiles')}>
+          {
+            TileWidths.map(([start, width], rowIndex) =>
+              <div key={rowIndex} >
+                {
+                  this.tiles.slice(start, start + width).map(tile => tile.render())
+                }
+              </div>
+            )
+          }
+        </div>
+      </div>
+      )
+    }
   }
-}
+)()
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,8 @@
-import Board from './Board';
+import board from './Board';
 
 import { cls } from './utils/utilities'
 import { Rerenderable } from './utils/Rerender'
 
-const board = new Board();
 console.log(board);
 
 export default function Game() {
