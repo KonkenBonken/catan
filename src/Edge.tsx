63ced20659673d26088146909cf18f94d9a39b50
KonkenBonken <konrad05pettersson@gmail.com>
2023-01-03 14:46:16 +0100

    Added neighboringEdges getter

--- src/Edge.tsx
+++ src/Edge.tsx
@@ -1,12 +1,50 @@
 import { Player } from './utils/enums'
 import { cls } from './utils/utilities'
 import rerender from './utils/Rerender'
+import board from './Board';
 
 export default class Edge {
   owner: Player | null = null
 
   get hasRoad() {
     return this.owner !== null;
+  }
+
+  get neighboringEdges() {
+    const myRow = board.edges.findIndex(row => row.includes(this)),
+      myCol = board.edges[myRow].findIndex(row => row === this),
+      tiltedRow = !(myRow % 2),
+      belowMid = myRow > 5,
+      isMid = myRow === 5,
+      neighbors: Edge[] = [];
+
+    if (tiltedRow) {
+      neighbors.push(board.edges[myRow][myCol - 1]);
+      neighbors.push(board.edges[myRow][myCol + 1]);
+
+      if (belowMid) {
+        neighbors.push(board.edges[myRow - 1]?.[Math.floor((myCol + 1) / 2)]);
+        neighbors.push(board.edges[myRow + 1]?.[Math.floor((myCol) / 2)]);
+      } else {
+        neighbors.push(board.edges[myRow - 1]?.[Math.floor((myCol) / 2)]);
+        neighbors.push(board.edges[myRow + 1]?.[Math.floor((myCol + 1) / 2)]);
+      }
+    } else {
+      neighbors.push(board.edges[myRow - 1]?.[Math.floor((myCol) * 2)]);
+      neighbors.push(board.edges[myRow + 1]?.[Math.floor((myCol) * 2)]);
+      if (isMid) {
+        neighbors.push(board.edges[myRow - 1]?.[Math.floor((myCol) * 2 - 1)]);
+        neighbors.push(board.edges[myRow + 1]?.[Math.floor((myCol) * 2 - 1)]);
+      }
+      else if (belowMid) {
+        neighbors.push(board.edges[myRow - 1]?.[Math.floor((myCol) * 2 + 1)]);
+        neighbors.push(board.edges[myRow + 1]?.[Math.floor((myCol) * 2 - 1)]);
+      } else {
+        neighbors.push(board.edges[myRow - 1]?.[Math.floor((myCol) * 2 - 1)]);
+        neighbors.push(board.edges[myRow + 1]?.[Math.floor((myCol) * 2 + 1)]);
+      }
+    }
+    return neighbors.filter(x => x);
   }
 
   render() {
