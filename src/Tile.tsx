27fc77c05eb5297ed5896153bfc4750022adbb05
KonkenBonken <konrad05pettersson@gmail.com>
2023-01-04 20:09:41 +0100

    Added neighboringEdges and -corners to Tile

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,10 +1,19 @@
 import { Resource } from './utils/enums'
 import { cls } from './utils/utilities'
 import Tileset from './Tileset128'
+import board from './Board'
 
 export default class Tile {
 
   constructor(readonly resource: Resource, readonly number: number) { }
+
+  get neighboringEdges() {
+    return board.edges.flat().filter(edge => edge.neighboringTiles.includes(this));
+  }
+
+  get neighboringCorners() {
+    return board.corners.flat().filter(corner => corner.neighboringTiles.includes(this));
+  }
 
   render() {
     return <div className={cls('tile')} >
