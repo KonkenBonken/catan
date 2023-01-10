15822885407a6ce62af12e81724bc905a901e214
KonkenBonken <konrad05pettersson@gmail.com>
2023-01-10 19:01:11 +0100

    Simplified tileDiv using ref

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -2,12 +2,9 @@ import { Resource, Building } from './utils/enums'
 import { cls } from './utils/utilities'
 import TileImage from './TileImage'
 import board from './Board'
-import rerender from './utils/Rerender';
-
-let currentTileId = 0;
 
 export default class Tile {
-  private readonly id = 't' + (currentTileId++).toString(36);
+  private tileDiv: HTMLDivElement | undefined;
 
   constructor(readonly resource: Resource, readonly number: number) { }
 
@@ -17,10 +14,6 @@ export default class Tile {
 
   get neighboringCorners() {
     return board.corners.flat().filter(corner => corner.neighboringTiles.includes(this));
-  }
-
-  get tileDiv() {
-    return document.querySelector(`.${cls('tile')}#${this.id}`) as HTMLDivElement | undefined;
   }
 
   giveResources() {
@@ -35,7 +28,7 @@ export default class Tile {
   }
 
   render() {
-    return <div className={cls('tile')} id={this.id}>
+    return <div className={cls('tile')} ref={div => this.tileDiv = div ?? undefined}>
       <TileImage resource={this.resource} />
       {!!this.number && <div className={cls('tileNumber', { marked: Math.abs(this.number - 7) === 1 })}>{this.number}</div>}
     </div>
