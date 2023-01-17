ebb89a1370b43003197bcb3a69ce28849f9a020c
KonkenBonken <konrad05pettersson@gmail.com>
2023-01-17 22:20:59 +0100

    Added knight styling

--- src/Tile.tsx
+++ src/Tile.tsx
@@ -5,8 +5,11 @@ import board from './Board'
 
 export default class Tile {
   private tileDiv: HTMLDivElement | undefined;
+  hasKnight: boolean;
 
-  constructor(readonly resource: Resource, readonly number: number) { }
+  constructor(readonly resource: Resource, readonly number: number) {
+    this.hasKnight = resource === Resource.Desert;
+  }
 
   get neighboringEdges() {
     return board.edges.flat().filter(edge => edge.neighboringTiles.includes(this));
@@ -29,6 +32,7 @@ export default class Tile {
 
   render() {
     return <div className={cls('tile')} ref={div => this.tileDiv = div ?? undefined}>
+      {this.hasKnight && <div className={cls('knight')} />}
       <TileImage resource={this.resource} />
       {!!this.number && <div className={cls('tileNumber', { marked: Math.abs(this.number - 7) === 1 })}>{this.number}</div>}
     </div>
